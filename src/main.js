b26eddb3105a7e6cbfcfd49afc595b81bb4c5312
suda hasemi <suda@rights-s.jp>
2021-06-07 17:08:09 +0900

    iPhoneX 対応

--- src/main.js
+++ src/main.js
@@ -2,19 +2,38 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 
 import Vue from 'vue';
-import '@/plugins/vue_onsenui_plugin';
+import VueOnsen from '@/plugins/vue_onsenui_plugin';
+import '@/plugins/global_component_plugin';
 import store from '@/stores';
 import App from '@/app';
 
 Vue.config.productionTip = false;
+
+// NOTE: enableAutoStatusBarFillは、$ons.isReady() = falseで動作するが、
+//       vueのbeforeCreateではtrueになってしまうため、先に実行する
+if (/iP(hone|(o|a)d)/.test(navigator.userAgent)) {
+  console.log('isIOS', VueOnsen);
+  VueOnsen.enableAutoStatusBarFill();
+}
+/* TODO: アプリ化対応時にonDeviceReadyに移動&書き換えてください
+onDeviceReady() {
+  if (window.device.platform === 'iOS') {
+    VueOnsen.enableAutoStatusBarFill();
+  }
+}
+*/
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   store,
   components: { App },
-  template: '<App/>',
-  beforeCreate () {
+  beforeCreate() {
     this.$ons.disableAutoStyling();
-  }
+
+    if (this.$ons.platform.isIPhoneX()) {
+      document.documentElement.setAttribute('onsflag-iphonex-portrait', '');
+    }
+  },
+  template: '<App/>',
 });
---
+++ src/plugins/global_component_plugin.js
@@ -0,0 +1,4 @@
+// 独自で作成したコンポーネント等の読み込み
+
+// customize style
+import '@/assets/scss/style.scss';
