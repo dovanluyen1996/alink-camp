5969ea07c139cb62a9de542b238116dbaa8f9fc9
suda hasemi <suda@rights-s.jp>
2021-06-07 11:59:04 +0900

    ons-toolbar, ons-tabbar cutomize

--- src/plugins/vue_onsenui_plugin.js
+++ src/plugins/vue_onsenui_plugin.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueOnsen from 'vue-onsenui';
 import 'onsenui/css/onsenui.css';
 import '@/assets/css/onsen-css-components.min.css';
+import '@/assets/scss/onsen-override/base-layout/index.scss';
 
 Vue.use(VueOnsen);
 
