a967635c4de373db60f8007260a777205df4cbeb
suda hasemi <suda@rights-s.jp>
2021-06-07 13:10:35 +0900

    onsenUIのデザインカスタマイズ(themeRollerでは変更できないもの)

--- src/plugins/vue_onsenui_plugin.js
+++ src/plugins/vue_onsenui_plugin.js
@@ -3,6 +3,7 @@ import VueOnsen from 'vue-onsenui';
 import 'onsenui/css/onsenui.css';
 import '@/assets/css/onsen-css-components.min.css';
 import '@/assets/scss/onsen-override/base-layout/index.scss';
+import '@/assets/scss/onsen-override/design/onsen-customize.scss';
 
 Vue.use(VueOnsen);
 
