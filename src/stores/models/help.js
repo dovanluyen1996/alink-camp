c6bf7f7031e2ea72ba8f58cadef426baa259cd0c
tranquanghuy-rightsvn <tranquang.huy@rights-s.jp>
2021-06-10 15:19:59 +0700

    Change getHelp

--- src/stores/models/help.js
+++ src/stores/models/help.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import Vue from 'vue';
-import ApiClient from '@/api_client';
-
-=======
 import ApiClient from '@/api_client';
 
 const attributes = () => (
@@ -14,12 +10,10 @@ const attributes = () => (
   }
 );
 
->>>>>>> 3c06657 (Handle store helps)
 export default {
   strict: true,
   namespaced: true,
   state: {
-<<<<<<< HEAD
     helps: [],
     isLoading: false,
   },
@@ -46,33 +40,6 @@ export default {
       } finally {
         context.commit('setIsLoading', false);
       }
-=======
-    ...attributes(),
-    helps: [],
-  },
-  mutations: {
-    setAttributes(state, help) {
-      Object.keys(attributes()).forEach((key) => {
-        state[key] = help[key];
-      });
-    },
-    setHelps(state, helps) {
-      state.helps = helps;
-    },
-  },
-  actions: {
-    async getHelp(context, helpId) {
-      context.commit('setIsLoading', true, { root: true });
-      const help = await ApiClient.getHelp(helpId);
-
-      context.commit('setAttributes', help);
-    },
-    async getHelps(context) {
-      context.commit('setIsLoading', true, { root: true });
-      const helps = await ApiClient.getHelps();
-
-      context.commit('setHelps', helps);
->>>>>>> 3c06657 (Handle store helps)
     },
   },
 };
