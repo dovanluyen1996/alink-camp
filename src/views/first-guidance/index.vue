6c2ceaadf7ea3ffbecbf9034b9f9b9eeb17e4032
suda hasemi <suda@rights-s.jp>
2021-06-11 15:03:08 +0900

    ダイアログを呼び出すメソッド追加とアラートダイアログのレイアウト調整 #14

--- src/views/first-guidance/index.vue
+++ src/views/first-guidance/index.vue
@@ -32,19 +32,39 @@
           </v-ons-card>
         </v-ons-carousel-item>
       </v-ons-carousel>
-    </div>
 
-    <fixed-footer>
-      <v-ons-button @click="clickNext">
-        次へ
-      </v-ons-button>
-    </fixed-footer>
+      <fixed-footer>
+        <v-ons-button @click="clickNext">
+          次へ
+        </v-ons-button>
+      </fixed-footer>
+
+      <v-ons-alert-dialog
+        modifier="rowfooter"
+        :visible.sync="isShownIdfaAlert"
+      >
+        <template #title>
+          ゴルフ天気にアプリとWEBの操作履歴の利用を許可しますか？
+        </template>
+        許可の設定により、今後も各種機能改善のご参考にさせていただきます。次へのボタン押下後のポップアップで選択してください
+        <template #footer>
+          <v-ons-button @click="callToIdfaDialog()">次へ</v-ons-button>
+        </template>
+      </v-ons-alert-dialog>
+    </div>
   </v-ons-page>
 </template>
 
 <script>
 // components
 import FixedFooter from '@/components/organisms/fixed-footer';
+
+const CAROUSEL_INDEXES = {
+  TOP: 0,
+  LOCATION_SERVICES: 1,
+  PUSH_NOTIFICATIONS: 2,
+  IDFA: 3,
+};
 
 export default {
   name: 'FIrstGuidance',
@@ -54,15 +74,52 @@ export default {
   data() {
     return {
       carouselIndex: 0,
+      isShownIdfaAlert: false,
     };
   },
   methods: {
     clickNext() {
-      if (this.carouselIndex === 3) {
-        console.log('goToUserNew');
-      } else {
-        this.carouselIndex += 1;
+      switch (this.carouselIndex) {
+      case CAROUSEL_INDEXES.TOP:
+        this.showNextCaroudel();
+        break;
+
+      case CAROUSEL_INDEXES.LOCATION_SERVICES:
+        this.callToLocationServicesDialog();
+        break;
+
+      case CAROUSEL_INDEXES.PUSH_NOTIFICATIONS:
+        this.callToPushNotificationDialog();
+        break;
+
+      case CAROUSEL_INDEXES.IDFA:
+        this.showIdfaAlert();
+        break;
+
+      default:
+        break;
       }
+    },
+    callToLocationServicesDialog() {
+      // TODO: location servicesのダイアログを出してください
+      console.log('callToLocationServicesDialog');
+      this.showNextCaroudel();
+    },
+    callToPushNotificationDialog() {
+      // TODO: Push notificationsのダイアログを出してください
+      console.log('callToLocationServicesDialog');
+      this.showNextCaroudel();
+    },
+    showIdfaAlert() {
+      this.isShownIdfaAlert = true;
+    },
+    callToIdfaDialog() {
+      this.isShownIdfaAlert = false;
+      // TODO: IDFAのダイアログを出してください
+      console.log('callToIdfaDialog');
+    },
+    showNextCaroudel() {
+      this.carouselIndex += 1;
     },
   },
 };
