a80cc261d69ca35eab7ad97de9c84dae0be32462
Le Duc Tien <leduc.tien@rights-s.jp>
2021-06-11 16:59:34 +0700

    remove api show

--- src/api_client/modules/notices_api.js
+++ src/api_client/modules/notices_api.js
@@ -3,8 +3,4 @@ export default {
     const response = await this.get('/notices');
     return response.data;
   },
-  async getNotice(noticeId) {
-    const response = await this.get(`/notices/${noticeId}`);
-    return response.data;
-  },
 };
