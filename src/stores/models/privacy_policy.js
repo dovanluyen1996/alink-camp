8abe51f4e62ba95ef808c13d7e16b2004a788fef
vuongvt-rights <vuong@rights-s.jp>
2021-06-14 16:16:51 +0700

    fix comment: isLoading is not object

--- src/stores/models/privacy_policy.js
+++ src/stores/models/privacy_policy.js
@@ -13,7 +13,7 @@ export default {
       Vue.set(state, 'privacyPolicy', privacyPolicy);
     },
     setIsLoading(state, isLoading) {
-      Vue.set(state, 'isLoading', isLoading);
+      state.isLoading = isLoading;
     },
   },
   actions: {
