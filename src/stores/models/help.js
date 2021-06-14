754b48c3d48fe0853293a3cc45d36ce19a21f6c6
vuongvt-rights <vuong@rights-s.jp>
2021-06-14 16:43:48 +0700

    fix yarn lint

--- src/stores/models/help.js
+++ src/stores/models/help.js
@@ -17,7 +17,7 @@ export default {
       Vue.set(state, 'helps', helps);
     },
     setIsLoading(state, isLoading) {
-      state.isLoading = isLoading
+      state.isLoading = isLoading;
     },
   },
   actions: {
