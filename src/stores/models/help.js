cb88cc37ab06c52d06d58a1bc9c2306e1e7ef03f
tranquanghuy-rightsvn <tranquang.huy@rights-s.jp>
2021-06-14 14:24:30 +0700

    Add store

---
+++ src/stores/models/help.js
@@ -0,0 +1,36 @@
+import Vue from 'vue';
+import ApiClient from '@/api_client';
+
+export default {
+  strict: true,
+  namespaced: true,
+  state: {
+    helps: [],
+    isLoading: false,
+  },
+  getters: {
+    all: state => state.helps,
+    findById: state => helpId => state.helps.find(help => help.id === helpId),
+  },
+  mutations: {
+    setHelps(state, helps) {
+      Vue.set(state, 'helps', helps);
+    },
+    setIsLoading(state, isLoading) {
+      Vue.set(state, 'isLoading', isLoading);
+    },
+  },
+  actions: {
+    async getHelps(context) {
+      context.commit('setIsLoading', true);
+
+      try {
+        const helps = await ApiClient.getHelps();
+
+        context.commit('setHelps', helps);
+      } finally {
+        context.commit('setIsLoading', false);
+      }
+    },
+  },
+};
