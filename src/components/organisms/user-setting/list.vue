1dda8ad1921b5486d6622200346581528ad88ed4
HaoNguyen <nguyenthi.hao@rights-s.jp>
2021-06-18 14:45:12 +0700

    add user-setting mock

---
+++ src/components/organisms/user-setting/list.vue
@@ -0,0 +1,147 @@
+<template>
+  <div class="user-setting-list">
+    <div
+      v-if="title"
+      class="user-setting-list-title"
+    >
+      {{ title }}
+    </div>
+
+    <v-ons-card>
+      <div class="content">
+        <v-ons-list modifier="noborder">
+          <v-ons-list-item
+            modifier="nodivider"
+          >
+            <div class="center">
+              <span class="list-item__title">
+                天気予報
+                <v-ons-switch
+                  v-model="settingValue.isReceivableWeatherForecast"
+                  data-model="isReceivableWeatherForecast"
+                  @change="event => setUserSetting(event)"
+                />
+              </span>
+              <span class="list-item__subtitle">
+                ※予定日当日の予報が発表された際にご連絡します（最速で10日前となります）
+              </span>
+            </div>
+          </v-ons-list-item>
+          <v-ons-list-item
+            modifier="nodivider"
+          >
+            <div class="center">
+              <span class="list-item__title">
+                予定日の天気を毎日ご連絡
+                <v-ons-switch
+                  v-model="settingValue.isReceivableDailyWhetherForecast"
+                  data-model="isReceivableDailyWhetherForecast"
+                  @change="event => setUserSetting(event)"
+                />
+              </span>
+              <span class="list-item__subtitle">
+                ※予定日10日前より定期的に該当コースの天気をご連絡します<br>
+                ※急な変化の際のキャンセルの判断にご利用ください
+              </span>
+            </div>
+          </v-ons-list-item>
+          <v-ons-list-item
+            modifier="nodivider"
+          >
+            <div class="center">
+              <span class="list-item__title">
+                落雷、雨雲警報
+                <v-ons-switch
+                  v-model="settingValue.isReceivableWarning"
+                  data-model="isReceivableWarning"
+                  @change="event => setUserSetting(event)"
+                />
+              </span>
+              <span class="list-item__subtitle">
+                ※予定日当日に該当コースで落雷警報が出た場合にご連絡します
+              </span>
+            </div>
+          </v-ons-list-item>
+        </v-ons-list>
+      </div>
+    </v-ons-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserSettingList',
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+    userSetting: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    settingValue: {
+      get() {
+        return { ...this.userSetting };
+      },
+      set(newValue) {
+        this.$emit('updateUserSetting', newValue);
+      },
+    },
+  },
+  methods: {
+    setUserSetting(event) {
+      const targetSettingModel = event.switch.dataset.model;
+      this.settingValue = { ...this.settingValue, [targetSettingModel]: event.value };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+@import '@/assets/scss/_mixins.scss';
+
+.card {
+  padding-top: 3px;
+  padding-bottom: 22px;
+}
+
+.user-setting-list-title {
+  @include title-style;
+
+  margin-top: 25px;
+
+  + .card {
+    margin-top: 14px;
+  }
+}
+
+.center {
+  padding-top: 12px;
+  padding-bottom: 12px;
+}
+
+.list-item {
+  &__title {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 12px;
+    font-size: 16px;
+    font-weight: 600;
+    line-height: 1.5;
+  }
+
+  &__subtitle {
+    font-size: $font-size-small;
+    color: $color-subtext;
+  }
+}
+
+.switch {
+  margin-left: 20px;
+}
+</style>
--- src/views/user-setting/index.vue
+++ src/views/user-setting/index.vue
@@ -1,48 +1,25 @@
 <template>
   <v-ons-page>
-    <div>
-      PUSH設定
-      <div>
-        天気予報
-        <input
-          v-model="userSetting.isReceivableWeatherForecast"
-          type="checkbox"
-          @change="updateUserSetting"
-        >
-        <div>
-          ※予定日当日の予報が発表された際にご連絡します最速で10日前となります）
-        </div>
-      </div>
-
-      <div>
-        予定日の天気を毎日ご通知
-        <input
-          v-model="userSetting.isReceivableDailyWhetherForecast"
-          type="checkbox"
-          @change="updateUserSetting"
-        >
-        <div>
-          ※予定日10日前より定期的に該当コースの天気をご連絡します <br>
-          ※急な変化の際のキャンセルの判断にご利用ください
-        </div>
-      </div>
-      <div>
-        落雷、雨雲警報
-        <input
-          v-model="userSetting.isReceivableWarning"
-          type="checkbox"
-          @change="updateUserSetting"
-        >
-        <div>
-          ※予定日当日に該当コースで落雷警報が出た場合にご連絡します
-        </div>
-      </div>
+    <custom-toolbar title="設定" />
+    <div class="content">
+      <user-setting-list
+        title="PUSH設定"
+        :user-setting="userSetting"
+        @updateUserSetting="updateUserSetting"
+      />
     </div>
   </v-ons-page>
 </template>
 
 <script>
+// components
+import UserSettingList from '@/components/organisms/user-setting/list';
+
 export default {
+  name: 'UserSetting',
+  components: {
+    UserSettingList,
+  },
   computed: {
     userSetting() {
       return this.$store.getters['models/userSetting/userSetting'];
@@ -55,9 +32,9 @@ export default {
     async getUserSetting() {
       await this.$store.dispatch('models/userSetting/getUserSetting');
     },
-    async updateUserSetting() {
+    async updateUserSetting(userSetting) {
       try {
-        await this.$store.dispatch('models/userSetting/updateUserSetting', this.userSetting);
+        await this.$store.dispatch('models/userSetting/updateUserSetting', userSetting);
       } catch (e) {
         this.getUserSetting();
       }
