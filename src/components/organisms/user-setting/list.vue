bcdbbaacce5516d309e04380da8d2a98e527ed23
HaoNguyen <nguyenthi.hao@rights-s.jp>
2021-06-22 14:34:19 +0700

    use cart-with-title component

--- src/components/organisms/user-setting/list.vue
+++ src/components/organisms/user-setting/list.vue
@@ -1,76 +1,70 @@
 <template>
-  <div class="user-setting-list">
-    <div
+  <card-with-title>
+    <template
       v-if="title"
-      class="user-setting-list-title"
+      slot="title"
     >
       {{ title }}
-    </div>
-
-    <v-ons-card>
-      <div class="content">
-        <v-ons-list modifier="noborder">
-          <v-ons-list-item
-            modifier="nodivider"
-          >
-            <div class="center">
-              <span class="list-item__title">
-                天気予報
-                <v-ons-switch
-                  v-model="settingValue.isReceivableWeatherForecast"
-                  data-model="isReceivableWeatherForecast"
-                  @change="event => setUserSetting(event)"
-                />
-              </span>
-              <span class="list-item__subtitle">
-                ※予定日当日の予報が発表された際にご連絡します（最速で10日前となります）
-              </span>
-            </div>
-          </v-ons-list-item>
-          <v-ons-list-item
-            modifier="nodivider"
-          >
-            <div class="center">
-              <span class="list-item__title">
-                予定日の天気を毎日ご連絡
-                <v-ons-switch
-                  v-model="settingValue.isReceivableDailyWhetherForecast"
-                  data-model="isReceivableDailyWhetherForecast"
-                  @change="event => setUserSetting(event)"
-                />
-              </span>
-              <span class="list-item__subtitle">
-                ※予定日10日前より定期的に該当コースの天気をご連絡します<br>
-                ※急な変化の際のキャンセルの判断にご利用ください
-              </span>
-            </div>
-          </v-ons-list-item>
-          <v-ons-list-item
-            modifier="nodivider"
-          >
-            <div class="center">
-              <span class="list-item__title">
-                落雷、雨雲警報
-                <v-ons-switch
-                  v-model="settingValue.isReceivableWarning"
-                  data-model="isReceivableWarning"
-                  @change="event => setUserSetting(event)"
-                />
-              </span>
-              <span class="list-item__subtitle">
-                ※予定日当日に該当コースで落雷警報が出た場合にご連絡します
-              </span>
-            </div>
-          </v-ons-list-item>
-        </v-ons-list>
-      </div>
-    </v-ons-card>
-  </div>
+    </template>
+    <v-ons-list modifier="noborder">
+      <v-ons-list-item modifier="nodivider">
+        <div class="center">
+          <span class="list-item__title">
+            天気予報
+            <v-ons-switch
+              v-model="settingValue.isReceivableWeatherForecast"
+              data-model="isReceivableWeatherForecast"
+              @change="event => setUserSetting(event)"
+            />
+          </span>
+          <span class="list-item__subtitle">
+            ※予定日当日の予報が発表された際にご連絡します（最速で10日前となります）
+          </span>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item modifier="nodivider">
+        <div class="center">
+          <span class="list-item__title">
+            予定日の天気を毎日ご連絡
+            <v-ons-switch
+              v-model="settingValue.isReceivableDailyWhetherForecast"
+              data-model="isReceivableDailyWhetherForecast"
+              @change="event => setUserSetting(event)"
+            />
+          </span>
+          <span class="list-item__subtitle">
+            ※予定日10日前より定期的に該当コースの天気をご連絡します<br>
+            ※急な変化の際のキャンセルの判断にご利用ください
+          </span>
+        </div>
+      </v-ons-list-item>
+      <v-ons-list-item modifier="nodivider">
+        <div class="center">
+          <span class="list-item__title">
+            落雷、雨雲警報
+            <v-ons-switch
+              v-model="settingValue.isReceivableWarning"
+              data-model="isReceivableWarning"
+              @change="event => setUserSetting(event)"
+            />
+          </span>
+          <span class="list-item__subtitle">
+            ※予定日当日に該当コースで落雷警報が出た場合にご連絡します
+          </span>
+        </div>
+      </v-ons-list-item>
+    </v-ons-list>
+  </card-with-title>
 </template>
 
 <script>
+import CardWithTitle from '@/components/organisms/card-with-title';
+
 export default {
   name: 'UserSettingList',
+  components: {
+    CardWithTitle,
+  },
   props: {
     title: {
       type: String,
@@ -79,6 +73,7 @@ export default {
     userSetting: {
       type: Object,
       default: () => {},
+      required: true,
     },
   },
   computed: {
@@ -102,21 +97,10 @@ export default {
 
 <style lang="scss" scoped>
 @import '@/assets/scss/_variables.scss';
-@import '@/assets/scss/_mixins.scss';
 
 .card {
   padding-top: 3px;
   padding-bottom: 22px;
-}
-
-.user-setting-list-title {
-  @include title-style;
-
-  margin-top: 25px;
-
-  + .card {
-    margin-top: 14px;
-  }
 }
 
 .center {
@@ -130,7 +114,7 @@ export default {
     align-items: center;
     justify-content: space-between;
     margin-bottom: 12px;
-    font-size: 16px;
+    font-size: $font-size-large;
     font-weight: 600;
     line-height: 1.5;
   }
