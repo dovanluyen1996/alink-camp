ab8f19b6c436bd1d4ca97e7d1112cdf67d9d25cf
TAKUMA KIMISHIMA <takuma.kimishima@rights-s.jp>
2021-06-23 20:05:35 +0900

    スコア一覧画面の実装

--- src/views/scores/index.vue
+++ src/views/scores/index.vue
@@ -1,8 +1,11 @@
 <template>
   <v-ons-page>
     <custom-toolbar title="コースお天気" />
-    <div class="content">
-      <no-data v-if="courses.length === 0">
+    <div
+      v-show="userCourses.length"
+      class="content"
+    >
+      <no-data v-if="userCourses.length === 0">
         <p>
           まだスコア登録がありません。<br>
           コース検索より、追加したいコースを選択してください
@@ -19,7 +22,7 @@
 
       <course-list
         v-else
-        :courses="courses"
+        :courses="userCourses"
         @click="goToCourseSearch"
       />
 
@@ -48,45 +51,13 @@ export default {
     CourseList,
     FixedFooter,
   },
-  data() {
-    return {
-      courses: [
-        {
-          id: 1,
-          poi_id: 1,
-          jalan_code: 'shintoukyou',
-          name: '新東京都民ゴルフ場',
-          name_kana: 'シントウキョウトミンゴルフジョウ',
-          address: '東京都足立区新田１丁目１５−１',
-          latitude: 0,
-          longitude: 0,
-          phone: '000-0000-0000',
-          phone2: '000-0000-0000',
-          url: 'http://',
-          jalan_description: 'じゃらんコース紹介',
-          jalan_image_path: 'jaran_image_path',
-          jalan_url: 'jaran_url',
-          note: '備考',
-        },
-        {
-          id: 2,
-          poi_id: 2,
-          jalan_code: 'shintoukyou',
-          name: '新東京都民ゴルフ場',
-          name_kana: 'シントウキョウトミンゴルフジョウ',
-          address: '東京都足立区新田１丁目１５−１',
-          latitude: 0,
-          longitude: 0,
-          phone: '000-0000-0000',
-          phone2: '000-0000-0000',
-          url: 'http://',
-          jalan_description: 'じゃらんコース紹介',
-          jalan_image_path: 'jaran_image_path',
-          jalan_url: 'jaran_url',
-          note: '備考',
-        },
-      ],
-    };
+  computed: {
+    userCourses() {
+      return this.$store.getters['models/userCourse/all'];
+    },
+  },
+  async created() {
+    await this.$store.dispatch('models/userCourse/getUserCourses');
   },
   methods: {
     goToCourseSearch() {
