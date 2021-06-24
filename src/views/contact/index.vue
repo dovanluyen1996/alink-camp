15fcf676249fdedcb0c006e34d837361eff3310c
tranquanghuy-rightsvn <tranquang.huy@rights-s.jp>
2021-06-24 16:49:05 +0700

    Handle validation and dialog

--- src/views/contact/index.vue
+++ src/views/contact/index.vue
@@ -1,26 +1,82 @@
 <template>
   <v-ons-page>
     <div>
-      <div>
-        お問い合わせ
+      <validation-observer
+        ref="guidanceForm"
+        v-slot="{ handleSubmit }"
+      >
+        <div>
+          お問い合わせ項目
+          <validation-provider
+            v-slot="{ errors }"
+            rules="required"
+          >
+            <select
+              v-model="contact.category"
+              name="カテゴリー"
+            >
+              <option
+                v-for="category in categories"
+                :key="category.index"
+              >
+                {{ category }}
+              </option>
+            </select>
+            <span>{{ errors[0] }}</span>
+          </validation-provider>
+        </div>
 
-        <!-- TODO: Add real category -->
-        <select v-model="contact.category">
-          <option>How to use app</option>
-          <option>Infomation</option>
-        </select>
-      </div>
+        <div>
+          内容
+          <validation-provider
+            v-slot="{ errors }"
+            rules="required"
+          >
+            <input
+              v-model="contact.message"
+              name="内容"
+            >
+            <span>{{ errors[0] }}</span>
+          </validation-provider>
+        </div>
 
-      <div>
-        内容
-        <input
-          v-model="contact.message"
+        <div>
+          留意事項
+          <input name="留意事項">
+        </div>
+
+        <v-ons-button
+          @click="handleSubmit(showConfirm)"
         >
-      </div>
+          送信確認
+        </v-ons-button>
+      </validation-observer>
 
-      <v-ons-button @click="sendContact">
-        送信確認
-      </v-ons-button>
+      <v-ons-alert-dialog
+        :visible.sync="confirmSendContact"
+        cancelable
+      >
+        <template #title>
+          送信確認
+        </template>
+
+        お問い合わせ内容を送信します。<br>
+        メールアドレスなど記載のある場合、お間違えがあると返信メールが届かない可能性があります。ご注意ください。
+
+        <template #footer>
+          <v-ons-button
+            modifier="quiet quiet-dark"
+            @click="closeConfirm()"
+          >
+            キャンセル
+          </v-ons-button>
+          <v-ons-button
+            @click="submit()"
+          >
+            応募する
+          </v-ons-button>
+        </template>
+      </v-ons-alert-dialog>
     </div>
   </v-ons-page>
 </template>
@@ -33,15 +89,26 @@ export default {
         message: '',
         category: '',
       },
+      categories: [
+        // TODO: Change real categories
+        'How to use app', 'Information',
+      ],
+      confirmSendContact: false,
     };
   },
   methods: {
-    async sendContact() {
-      // TODO: Add real information app when release
-      const appVersion = '1';
-      const osVersion = '14.3';
-      const deviceName = 'iphone5';
-      const platform = 'iOS';
+    showConfirm() {
+      this.confirmSendContact = true;
+    },
+    closeConfirm() {
+      this.confirmSendContact = false;
+    },
+    async submit() {
+      // TODO: change app version
+      const appVersion = '1.0';
+      const osVersion = window.device.version;
+      const deviceName = window.device.model;
+      const { platform } = window.device;
       const params = {
         ...this.contact,
         deviceName,
@@ -49,6 +116,7 @@ export default {
         osVersion,
         platform,
       };
+      this.closeConfirm();
       await this.$store.dispatch('models/contact/sendContact', params);
     },
   },
