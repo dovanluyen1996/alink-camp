489dab13ddcb6da43c424f08aecebf5fa7c8ff1f
HaoNguyen <nguyenthi.hao@rights-s.jp>
2021-06-25 10:11:31 +0700

    add mock for help-detail page

---
+++ src/components/organisms/help/detail.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-ons-card fullscreen>
+    <div class="content">
+      <div class="help-detail">
+        <img
+          v-if="help.image.url"
+          :src="help.image.url"
+          class="help-detail__thumbnail"
+        >
+        <!-- eslint-disable vue/no-v-html -->
+        <div
+          class="help-detail__description"
+          v-html="help.description"
+        />
+        <!-- eslint-enable vue/no-v-html -->
+      </div>
+    </div>
+  </v-ons-card>
+</template>
+
+<script>
+export default {
+  name: 'HelpDetail',
+  props: {
+    help: {
+      type: Object,
+      default: () => {},
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+
+.help-detail {
+  &__thumbnail {
+    width: 100%;
+    height: 206px;
+    margin-top: 5px;
+    margin-bottom: 20px;
+    border-radius: 10px;
+    object-fit: cover;
+  }
+
+  &__description {
+    display: block;
+    font-weight: 300;
+    line-height: 1.7;
+    white-space: pre-line;
+  }
+}
+</style>
--- src/views/helps/show.vue
+++ src/views/helps/show.vue
@@ -1,27 +1,21 @@
 <template>
   <v-ons-page>
-    <v-ons-toolbar>
-      <div class="left">
-        <v-ons-back-button />
-      </div>
-      <div class="center">
-        {{ help.title }}
-      </div>
-    </v-ons-toolbar>
-    <v-ons-card>
-      <div>
-        <div>
-          <img :src="help.image.url">
-        </div>
-        <div>
-          {{ help.description }}
-        </div>
-      </div>
-    </v-ons-card>
+    <custom-toolbar :title="help.title" />
+
+    <div class="content">
+      <help-detail :help="help" />
+    </div>
   </v-ons-page>
 </template>
+
 <script>
+import HelpDetail from '@/components/organisms/help/detail';
+
 export default {
+  name: 'HelpShow',
+  components: {
+    HelpDetail,
+  },
   props: {
     helpId: {
       type: Number,
