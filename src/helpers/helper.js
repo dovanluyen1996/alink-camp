e528a637aaaa367fca2c0a6205c48989d45c525f
Natsuru Shirose <natsuru.shirose@rights-s.jp>
2021-07-01 18:13:04 +0900

    helper 修正

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,17 +1,12 @@
+import moment from 'moment';
+
+moment.locale('ja');
+
 export default {
   toLongString(targetDate) {
-    const date = new Date(targetDate);
-    const year = date.getFullYear();
-    const month = (`00${(date.getMonth() + 1)}`).slice(-2);
-    const day = (`00${date.getDate()}`).slice(-2);
-    const dayOfWeekStr = new Intl.DateTimeFormat('ja-JP', { weekday: 'short' }).format(date);
-    return `${year}/${month}/${day} (${dayOfWeekStr})`;
+    return moment(targetDate).format('YYYY/MM/DD (dd)');
   },
   toShortString(targetDate) {
-    const date = new Date(targetDate);
-    const month = date.getMonth() + 1;
-    const day = (`00${date.getDate()}`).slice(-2);
-    const dayOfWeekStr = new Intl.DateTimeFormat('ja-JP', { weekday: 'short' }).format(date);
-    return `${month}/${day} (${dayOfWeekStr})`;
+    return moment(targetDate).format('M/DD (dd)');
   },
 };
