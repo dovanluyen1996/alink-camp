3575647ba0dd26cd3cbfda98a0400f26afd7e41d
vuongvt-rights <vuong@rights-s.jp>
2021-07-01 15:59:38 +0700

    fix rebase

--- src/stores/models/course.js
+++ src/stores/models/course.js
@@ -19,23 +19,6 @@ export default {
     setCourses(state, courses) {
       Vue.set(state, 'courses', courses);
     },
-    setViewedCourses(state, courses) {
-      Vue.set(state, 'viewedCourses', courses);
-    },
-    addViewedCourse(state, course) {
-      // If exsited viewed, don't add course to Store
-      const exsitedViewedCourse = state.viewedCourses.find(viewed => viewed.id === course.id);
-      if (exsitedViewedCourse) return;
-
-      // 閲覧履歴 will show 5 items, therefore if current 閲覧履歴 is 5, delete the first item
-      if (state.viewedCourses.length === 5) {
-        state.viewedCourses.splice(0, 1);
-      }
-      state.viewedCourses.push(course);
-
-      // Update 閲覧履歴 local storage
-      localStorage.setItem('viewedCourses', JSON.stringify(state.viewedCourses));
-    },
   },
   actions: {
     async getCourses(context, params) {
@@ -51,13 +34,6 @@ export default {
     },
     resetCourses(context) {
       context.commit('setCourses', []);
-    },
-    addViewedCourses(context, course) {
-      context.commit('addViewedCourse', course);
-    },
-    getViewedCourses(context) {
-      // Get viewed courses from Local Storage
-      context.commit('setViewedCourses', JSON.parse(localStorage.getItem('viewedCourses')) || []);
     },
   },
 };
