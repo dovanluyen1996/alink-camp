5c0220be214029530f5dc9987a6ea0ae6c50e8bf
HaoNguyen <nguyenthi.hao@rights-s.jp>
2021-07-09 09:23:53 +0700

    add prefix for background images

--- src/components/organisms/wind-forecast/compass.vue
+++ src/components/organisms/wind-forecast/compass.vue
@@ -60,7 +60,7 @@ $degrees: 0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5, 180, 202.5, 225, 247.5, 270,
   justify-content: center;
   width: 320px;
   height: 320px;
-  background-image: url('~@/assets/images/weathers/compass.png');
+  background-image: url('~@/assets/images/weathers/background-compass.png');
   background-position: center;
   background-size: cover;
 }
@@ -83,15 +83,15 @@ $degrees: 0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5, 180, 202.5, 225, 247.5, 270,
   background-size: cover;
 
   &--normal {
-    background-image: url('~@/assets/images/weathers/wind-speed/normal.png');
+    background-image: url('~@/assets/images/weathers/wind-speed/background-normal.png');
   }
 
   &--strong {
-    background-image: url('~@/assets/images/weathers/wind-speed/strong.png');
+    background-image: url('~@/assets/images/weathers/wind-speed/background-strong.png');
   }
 
   &--danger {
-    background-image: url('~@/assets/images/weathers/wind-speed/danger.png');
+    background-image: url('~@/assets/images/weathers/wind-speed/background-danger.png');
   }
 }
 
