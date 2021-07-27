991dd4775897e3378f63699af6653fd7185d056e
TAKUMA KIMISHIMA <takuma.kimishima@rights-s.jp>
2021-07-27 21:13:08 +0900

    stylelint auto fix

--- src/components/atoms/form/custom-image-upload.vue
+++ src/components/atoms/form/custom-image-upload.vue
@@ -184,10 +184,10 @@ export default {
 }
 
 .share-button {
-  height: 24px;
-  font-size: 12px;
   min-width: 140px;
-  line-height: 16px;
+  height: 24px;
   margin-top: 10px;
+  font-size: 12px;
+  line-height: 16px;
 }
 </style>
