3c16e2e65119e220723de520d010eb96224987e7
nozomu-rights <nozomu.otsuka@rights-s.jp>
2021-09-10 11:13:38 +0900

    課金処理を共通化（実装中）

--- src/app.vue
+++ src/app.vue
@@ -8,12 +8,37 @@
 <script>
 import AppNavigator from '@/views/app-navigator';
 import ApiErrorDialog from '@/components/organisms/api-error-dialog';
+import PurchaseInformation from '@/views/purchase-information';
 
 export default {
   name: 'App',
   components: {
     AppNavigator,
     ApiErrorDialog,
+  },
+  mounted() {
+    document.addEventListener('resume', this.onResume, false);
+  },
+  methods: {
+    async onResume() {
+      if (!BuildInfo.debug && !this.checkChargedStatus()) {
+        await this.$store.dispatch('appNavigator/push', PurchaseInformation);
+      }
+    },
+    async checkChargedStatus() {
+      Purchases.getPurchaserInfo(
+        async(purchaserInfo) => {
+          const isCharged = Object.entries(purchaserInfo.entitlements.active).length > 0;
+          this.checkChargedStatusComplete(isCharged);
+
+        },
+        () => {
+          // 全画面共通のエラーメッセージを表示したい
+          this.checkChargedStatusErrorVisible = true;
+          this.checkChargedStatusComplete(false);
+        },
+      );
+    },
   },
 };
 </script>
