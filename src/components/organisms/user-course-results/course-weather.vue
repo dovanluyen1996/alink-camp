ae5c905bc25b34b0e721774e95f2acc9a6503987
tranquanghuy-rightsvn <tranquang.huy@rights-s.jp>
2021-09-14 10:26:13 +0700

    Use component date-row

--- src/components/organisms/user-course-results/course-weather.vue
+++ src/components/organisms/user-course-results/course-weather.vue
@@ -1,24 +1,7 @@
 <template>
   <sticky-table class="score-course-weather">
     <template #thead>
-      <tr>
-        <th class="th">
-          日
-        </th>
-        <th
-          v-for="(weather, index) in weathers"
-          :key="index"
-          scope="col"
-          class="th"
-          :class="[saturdayCol(weather.date), sundayCol(weather.date)]"
-        >
-          <span
-            v-if="weather"
-          >
-            {{ displayDate(weather.date) }}
-          </span>
-        </th>
-      </tr>
+      <date-row :forecast-data="weathers" />
     </template>
     <weather-row :weathers="weathers" />
     <temperature-row
@@ -37,6 +20,7 @@
 import StickyTable from '@/components/organisms/sticky-table';
 import WeatherRow from '@/components/organisms/weather-table/weather-row';
 import TemperatureRow from '@/components/organisms/weather-table/temperature-row';
+import DateRow from '@/components/organisms/weather-table/date-row';
 
 export default {
   name: 'UserCourseReslutsCourseWeather',
@@ -44,22 +28,12 @@ export default {
     StickyTable,
     WeatherRow,
     TemperatureRow,
+    DateRow,
   },
   props: {
     weathers: {
       type: Array,
       required: true,
-    },
-  },
-  methods: {
-    displayDate(date) {
-      return this.$helpers.toWeekDayString(date);
-    },
-    saturdayCol(date) {
-      return this.$helpers.isSaturday(date) ? 'date-row__sarturday' : '';
-    },
-    sundayCol(date) {
-      return this.$helpers.isSunday(date) ? 'date-row__sunday' : '';
     },
   },
 };
@@ -81,13 +55,14 @@ export default {
       table-layout: fixed;
     }
 
+    tr {
+      td {
+        width: 30px;
+      }
+    }
+
     tbody {
       th {
-        width: 50px;
-        min-width: 50px;
-      }
-
-      th:nth-child(1) {
         width: 30px;
         min-width: 30px;
       }
@@ -96,26 +71,6 @@ export default {
         background: $color-th;
       }
     }
-  }
-}
-
-.th {
-  background: #fff;
-}
-
-.th:nth-child(1) {
-  background: $color-th;
-}
-
-.date-row {
-  &__sarturday {
-    color: #113095;
-    background-color: #e3f6ff;
-  }
-
-  &__sunday {
-    color: #9d1d1d;
-    background-color: #ffeaea;
   }
 }
 </style>
