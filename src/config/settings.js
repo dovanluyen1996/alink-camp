397de9d2c1dd76fceba34fc0bfa5cd8c65016b42
vuongvt-rights <vuong@rights-s.jp>
2021-10-06 12:21:39 +0700

    remove TODO comment

--- src/config/settings.js
+++ src/config/settings.js
@@ -21,7 +21,6 @@ export default {
     password: /^(?=.*?[a-z])(?=.*?\d).{6,}$/,
   },
   app_store: {
-    // TODO: Change real link
     ios: 'https://itunes.apple.com/jp/app/id1584902038?mt=8',
     android: 'https://play.google.com/store/apps/details?id=jp.or.jwa.tenkijp.golf',
   },
