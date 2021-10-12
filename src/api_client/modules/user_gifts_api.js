fa2a443c7d954fdd042a8f04695e35b44ba5be47
Akira Yoshida (Noguchi) <guppy0356.nakira@gmail.com>
2021-10-12 17:35:51 +0900

    GET /api/user_gifts にアクセスする ApliClient を追加

--- src/api_client/modules/user_gifts_api.js
+++ src/api_client/modules/user_gifts_api.js
@@ -3,4 +3,8 @@ export default {
     const response = await this.post('/user_gifts', params);
     return response.data;
   },
+  async getUserGifts() {
+    const response = await this.get('/user_gifts');
+    return response.data;
+  }
 };
