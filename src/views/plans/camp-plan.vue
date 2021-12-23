d6c6b34e00ea7e0de0708e3493f982c9b522bf3a
vanluyen <dovan.luyen@rights-s.jp>
2021-12-23 11:41:33 +0700

    screen camp plan

---
+++ src/views/plans/camp-plan.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-ons-page>
+    <custom-toolbar title="計画一覧" />
+    <div class="content">
+      <add-plan />
+    </div>
+  </v-ons-page>
+</template>
+
+<script setup>
+import AddPlan from '@/components/organisms/plan/add-plan';
+
+export default {
+  components: {
+    AddPlan,
+  },
+};
+</script>
