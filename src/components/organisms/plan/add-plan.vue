f06a2fa44fe2f7decc2c6b8130fb3961d2b8d67a
vanluyen <dovan.luyen@rights-s.jp>
2021-12-23 08:41:27 +0700

    update screen add plan

--- src/components/organisms/plan/add-plan.vue
+++ src/components/organisms/plan/add-plan.vue
@@ -2,7 +2,7 @@
   <v-ons-page>
     <v-ons-col class="text">
       <v-ons-row class="text__desc">
-        〇〇〇〇〇〇〇〇〇〇〇〇<br />〇〇〇〇〇〇〇〇〇〇〇〇キャンズ場
+        〇〇〇〇〇〇〇〇〇〇〇〇<br>〇〇〇〇〇〇〇〇〇〇〇〇キャンズ場
       </v-ons-row>
     </v-ons-col>
     <v-ons-card>
@@ -10,22 +10,60 @@
         class="button--red button--more"
       >
         <img
-          :src="require('@/assets/images/form/icon-add.png')" width="24px" height="24px"
+          :src="require('@/assets/images/form/icon-add.png')"
+          width="24px"
+          height="24px"
         >
         新規計画の追加
       </v-ons-button>
 
-      <time-plan />
+      <time-plan
+        :presents="presents"
+        :pasts="pasts"
+      />
     </v-ons-card>
   </v-ons-page>
 </template>
 
 <script>
-import TimePlan from '@/components/organisms/time-plan.vue'
+import TimePlan from '@/components/organisms/time-plan.vue';
 
 export default {
   components: {
-    TimePlan
+    TimePlan,
+  },
+  data() {
+    return {
+      presents: [
+        {
+          id: 1,
+          text: '2021/11/30（金)',
+        },
+        {
+          id: 2,
+          text: '2021/11/30（金)',
+        },
+      ],
+
+      pasts: [
+        {
+          id: 1,
+          text: '2021/11/30（金)',
+        },
+        {
+          id: 2,
+          text: '2021/11/30（金)',
+        },
+        {
+          id: 3,
+          text: '2021/11/30（金)',
+        },
+        {
+          id: 4,
+          text: '2021/11/30（金)',
+        },
+      ],
+    };
   },
 };
 </script>
@@ -42,6 +80,7 @@ export default {
     display: grid;
     justify-content: center;
     background-color: #fff;
+
     &__desc {
       padding: 10px;
       font-size: 18px;
--- src/components/organisms/time-plan.vue
+++ src/components/organisms/time-plan.vue
@@ -1,49 +1,33 @@
 <template>
   <v-ons-card>
     <v-ons-list modifier="noborder">
-      <p class="list__title">終了前の計画</p>
+      <p class="list__title">
+        終了前の計画
+      </p>
       <v-ons-list-item
+        v-for="time in presents"
+        :key="time.id"
         modifier="longdivider chevron"
-        @click="clickPrefecture(prefecture)"
+        @click="goToPlanPresent"
       >
         <div class="center">
-          <span class="list-item__title">2021/11/30（金）</span>
-        </div>
-      </v-ons-list-item>
-      <v-ons-list-item
-        modifier="longdivider chevron"
-        @click="clickPrefecture(prefecture)"
-      >
-        <div class="center">
-          <span class="list-item__title">2021/11/30（金）</span>
+          <span class="list-item__title">{{ time.text }}</span>
         </div>
       </v-ons-list-item>
     </v-ons-list>
 
     <v-ons-list modifier="noborder">
-      <p class="list__title">過去の計画</p>
+      <p class="list__title">
+        過去の計画
+      </p>
       <v-ons-list-item
+        v-for="time in pasts"
+        :key="time.id"
         modifier="longdivider chevron"
-        @click="clickPrefecture(prefecture)"
+        @click="goToPlanPast"
       >
         <div class="center">
-          <span class="list-item__title">2021/11/30（金）</span>
-        </div>
-      </v-ons-list-item>
-      <v-ons-list-item
-        modifier="longdivider chevron"
-        @click="clickPrefecture(prefecture)"
-      >
-        <div class="center">
-          <span class="list-item__title">2021/11/30（金）</span>
-        </div>
-      </v-ons-list-item>
-      <v-ons-list-item
-        modifier="longdivider chevron"
-        @click="clickPrefecture(prefecture)"
-      >
-        <div class="center">
-          <span class="list-item__title">2021/11/30（金）</span>
+          <span class="list-item__title">{{ time.text }}</span>
         </div>
       </v-ons-list-item>
     </v-ons-list>
@@ -54,10 +38,23 @@
 
 export default {
   name: 'TimePlan',
-  data() {
+  props: {
+    presents: {
+      type: Array,
+      default: () => [],
+    },
+    pasts: {
+      type: Array,
+      default: () => [],
+    },
   },
   methods: {
-    
+    goToPlanPresent() {
+      // TODO: Redirect to Plan Present
+    },
+    goToPlanPast() {
+      // TODO: Redirect to Plan Past
+    },
   },
 };
 </script>
@@ -65,12 +62,13 @@ export default {
 <style lang="scss" scoped>
 @import '@/assets/scss/_variables.scss';
 
-::v-deep {
+/deep/ {
   .card {
-    text-align: center;
     padding-top: 5px;
     padding-bottom: 0;
+    text-align: center;
   }
+
   .list {
     &__title {
       font-size: 18px;
@@ -85,20 +83,19 @@ export default {
 }
 
 .list-item {
-  border-bottom: 1px solid #d9d9d9;
   border-top: 1px solid #d9d9d9;
+  border-bottom: 1px solid #d9d9d9;
 
   &__title {
-    text-align: center;
     margin-bottom: 5px;
     font-size: 18px;
     line-height: 1.2;
+    text-align: center;
   }
 
   &__subtitle {
     font-size: $font-size-small;
     color: $color-subtext;
   }
-  
 }
 </style>
