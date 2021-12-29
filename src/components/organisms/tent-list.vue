a89d8a9354c567ca4bbccf62a8db48dc49557d47
Hung <nguyenxuan.hung@rights-s.jp>
2021-12-29 16:58:48 +0700

    fix: delete import scss files

--- src/components/organisms/tent-list.vue
+++ src/components/organisms/tent-list.vue
@@ -46,9 +46,6 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/scss/_variables.scss';
-@import '@/assets/scss/_mixins.scss';
-
 .tent-list {
   &[fullscreen] {
     height: 100%;
