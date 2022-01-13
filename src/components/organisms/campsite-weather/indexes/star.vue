979e6d1f28accb6188ac4525ed8306a33b83bf1f
Akira Yoshida (Noguchi) <guppy0356.nakira@gmail.com>
2022-01-13 11:51:44 +0900

    キャンプ場の表示に必要なコンポーネント郡を追加

---
+++ src/components/organisms/campsite-weather/indexes/star.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="star">
+    <div class="star-title">
+      星空指数
+    </div>
+    <img
+      :src="image"
+      width="26px"
+    >
+    <div class="star-label">
+      {{ forecast ? forecast.starTelop : '' }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CampsiteWeatherStarIndex',
+  props: {
+    forecast: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    image() {
+      if (!this.forecast) return this.$helpers.getImage('weathers/star/00.png');
+
+      const image = this.$helpers.getImage(`weathers/star/${this.forecast.starImageName}`);
+      return image || this.$helpers.getImage('weathers/star/00.png');
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_mixins.scss';
+@import '@/assets/scss/_variables.scss';
+
+.star-title {
+  @include title-style;
+
+  font-size: $font-size-small;
+}
+
+.star-label {
+  font-size: $font-size-small;
+}
+</style>
---
+++ src/components/organisms/campsite-weather/plan.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :class="['campsite-weather-plan', {'is-today' : isToday}]">
+    {{ plan.startedDate | moment('YYYY/MM/DD') }}〜
+    <span class="campsite-weather-plan__count-down">
+      {{ showCountDown }}
+    </span>
+  </div>
+</template>
+
+<script>
+import moment from 'moment';
+
+export default {
+  name: 'CampsiteWeatherPlan',
+  props: {
+    plan: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    isToday() {
+      const startedDate = moment(this.plan.startedDate).startOf('days');
+      const today = moment().startOf('days');
+
+      return startedDate.isSame(today);
+    },
+    showCountDown() {
+      if (this.isToday) return '予定日当日';
+
+      const startedDate = moment(this.plan.startedDate).startOf('days');
+      const today = moment().startOf('days');
+      const remainingDays = startedDate.diff(today, 'days');
+
+      return `予定日まで${remainingDays}日`;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables.scss';
+
+.campsite-weather-plan {
+  padding: 5px 10px;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  font-size: $font-size-small;
+  background: #f2f2f2;
+
+  &__count-down {
+    margin-left: 10px;
+    font-weight: 600;
+  }
+
+  &.is-today {
+    background: #fff0f0;
+
+    .campsite-weather-plan__count-down {
+      color: #eb2727;
+    }
+  }
+}
+</style>
