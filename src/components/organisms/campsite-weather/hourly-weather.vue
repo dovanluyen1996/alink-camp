21edca61a1f89b32b1f2a438d5126dd55d79c572
Akira Yoshida (Noguchi) <guppy0356.nakira@gmail.com>
2022-01-13 11:51:44 +0900

    キャンプ場の表示に必要なコンポーネント郡を追加

---
+++ src/components/organisms/campsite-weather/hourly-weather.vue
@@ -0,0 +1,123 @@
+<template>
+  <table class="campsite-weather-time-table">
+    <thead>
+      <tr>
+        <th
+          scope="col"
+          class="th"
+        >
+          時間
+        </th>
+        <th
+          scope="col"
+          class="th"
+        >
+          00 - 06
+        </th>
+        <th
+          scope="col"
+          class="th"
+        >
+          06 - 12
+        </th>
+        <th
+          scope="col"
+          class="th"
+        >
+          12 - 18
+        </th>
+        <th
+          scope="col"
+          class="th"
+        >
+          18 - 24
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <prob-precip-row
+        :prob-precips="probPrecips"
+        :color-th="false"
+      />
+      <wind-direction-row
+        :wind-directions="windDirections"
+        :color-th="false"
+      />
+      <wind-speed-row
+        :wind-speeds="windSpeeds"
+        :color-th="false"
+        :display-campsite-weather-top="true"
+      />
+    </tbody>
+  </table>
+</template>
+
+<script>
+// components
+import ProbPrecipRow from '@/components/organisms/weather-table/prob-precip-row';
+import WindDirectionRow from '@/components/organisms/weather-table/wind-direction-row';
+import WindSpeedRow from '@/components/organisms/weather-table/wind-speed-row';
+
+export default {
+  name: 'CampsiteWeatherHourlyWeather',
+  components: {
+    ProbPrecipRow,
+    WindDirectionRow,
+    WindSpeedRow,
+  },
+  props: {
+    forecast: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    probPrecips() {
+      if (!this.forecast) return [null, null, null, null];
+
+      const precips = [];
+
+      precips.push(this.forecast.precip_0);
+      precips.push(this.forecast.precip_6);
+      precips.push(this.forecast.precip_12);
+      precips.push(this.forecast.precip_18);
+
+      return precips;
+    },
+    windDirections() {
+      if (!this.forecast) return [null, null, null, null];
+
+      const drectoins = [];
+
+      drectoins.push(this.forecast.windDirection_0);
+      drectoins.push(this.forecast.windDirection_6);
+      drectoins.push(this.forecast.windDirection_12);
+      drectoins.push(this.forecast.windDirection_18);
+
+      return drectoins;
+    },
+    windSpeeds() {
+      if (!this.forecast) return [null, null, null, null];
+
+      const speeds = [];
+
+      speeds.push(this.forecast.windSpeed_0);
+      speeds.push(this.forecast.windSpeed_6);
+      speeds.push(this.forecast.windSpeed_12);
+      speeds.push(this.forecast.windSpeed_18);
+
+      return speeds;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables.scss';
+
+.campsite-weather-time-table {
+  width: 100%;
+  font-size: $font-size-small;
+  text-align: center;
+}
+</style>
---
+++ src/components/organisms/campsite-weather/indexes/thunder.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="thunder">
+    <div class="thunder-title">
+      落雷
+    </div>
+    <img
+      :src="image"
+      width="26px"
+    >
+    <div class="thunder-label">
+      {{ label }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CampsiteWeatherThunderIndex',
+  props: {
+    forecast: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      thunder_text: {
+        1: '低い',
+        2: 'やや低い',
+        3: 'やや高い',
+        4: '高い',
+      },
+    };
+  },
+  computed: {
+    image() {
+      if (!this.forecast) return this.$helpers.getImage('weathers/thunder/thunder_large_none.png');
+
+      const image = this.$helpers.getImage(`weathers/thunder/thunder_large_${this.forecast.thunderIndex}.png`);
+      return image || this.$helpers.getImage('weathers/thunder/thunder_large_none.png');
+    },
+    label() {
+      if (!this.forecast) return '';
+
+      return this.forecast.thunderIndex ? this.thunder_text[this.forecast.thunderIndex] : '';
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_mixins.scss';
+@import '@/assets/scss/_variables.scss';
+
+.thunder-title {
+  @include title-style;
+
+  font-size: $font-size-small;
+}
+
+.thunder-label {
+  font-size: $font-size-small;
+}
+</style>
