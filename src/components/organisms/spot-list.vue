a777eadd950031517d80b82d6aa90fd96fbfed51
Akira Yoshida (Noguchi) <guppy0356.nakira@gmail.com>
2022-01-24 21:35:01 +0900

    スポットの一覧を表示するコンポーネントを追加

---
+++ src/components/organisms/spot-list.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="spot-list">
+    <v-ons-card :fullscreen="isFullscreen">
+      <div class="content">
+        <v-ons-list>
+          <v-ons-list-item
+            v-for="spot in spots"
+            :key="spot.id"
+          >
+            <div class="center">
+              <div class="list-item__title">
+                <span>
+                  {{ spot.name }} ({{ Math.round(spot.distance) }}km)
+                </span>
+              </div>
+              <span class="list-item__subtitle">{{ spot.address }}</span>
+            </div>
+          </v-ons-list-item>
+        </v-ons-list>
+      </div>
+    </v-ons-card>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SpotList',
+  props: {
+    spots: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      isFullscreen: false,
+    };
+  },
+  mounted() {
+    this.setFullscreen();
+  },
+  methods: {
+    setFullscreen() {
+      // NOTE: propsにしたいところだけれど、
+      //       onsenUIが全画面表示をfullscreen属性でやっているので
+      //       合わせたほうがわかりやすいためDOMから探している
+      const isFullscreen = this.$el.getAttribute('fullscreen');
+      if (isFullscreen !== null) {
+        this.isFullscreen = true;
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/scss/_variables.scss';
+@import '@/assets/scss/_mixins.scss';
+
+.spot-list {
+  &[fullscreen] {
+    height: 100%;
+    overflow: hidden;
+  }
+}
+
+.card {
+  padding-top: 5px;
+  padding-bottom: 0;
+}
+
+.center {
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+
+.list-item {
+  &__title {
+    display: flex;
+    align-items: center;
+    margin-bottom: 5px;
+    font-size: 18px;
+    line-height: 1.2;
+  }
+
+  &__subtitle {
+    font-size: $font-size-small;
+    color: $color-subtext;
+  }
+}
+</style>
---
+++ src/views/spot-search/search-result.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-ons-page
+    :infinite-scroll="search"
+    @show="show"
+  >
+    <custom-toolbar :title="周辺検索" />
+    <div class="content">
+      <no-data v-if="spots.length === 0">
+        条件に合うコースが見つかりません
+      </no-data>
+
+      <spot-list
+        v-else
+        :spots="spots"
+      />
+    </div>
+  </v-ons-page>
+</template>
+
+<script>
+// components
+import NoData from '@/components/organisms/no-data';
+import SpotList from '@/components/organisms/spot-list';
+
+export default {
+  name: 'SpotSearchResult',
+  components: {
+    NoData,
+    SpotList,
+  },
+  props: {
+    location: {
+      type: Object,
+      default: () => null,
+    },
+  },
+  data() {
+    return {
+      page: 1,
+    };
+  },
+  computed: {
+    spots() {
+      return this.$store.getters['models/spot/all'];
+    },
+    totalCount() {
+      return this.$store.getters['models/spots/totalCount'];
+    },
+  },
+  methods: {
+    async search(done) {
+      // if getted spots is the same with total count, finish paging
+      if (this.totalCount === this.$store.getters['models/spot/size']) {
+        if (done) done();
+        return;
+      }
+
+      const searchParams = {
+        latitude: this.location.latitude,
+        longitude: this.location.longitude,
+        page: this.page,
+      };
+
+      await this.$store.dispatch('models/spot/getSpots', searchParams)
+        .then(() => {
+          this.page += 1;
+          if (done) done();
+        });
+    },
+    async show() {
+      this.$store.dispatch('appTabbar/setLastVisitedAt', this.$helpers.localDateWithHyphenFrom(new Date()));
+
+      const searchParams = {
+        latitude: this.location.latitude,
+        longitude: this.location.longitude,
+        page: this.page,
+      };
+
+      await this.$store.dispatch('models/spot/getSpots', searchParams)
+        .then(() => {
+          this.page += 1;
+        });
+    },
+  },
+};
+</script>
