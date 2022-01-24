42040801dbd8ddf22c70126e6b36d14cf78d086f
Akira Yoshida (Noguchi) <guppy0356.nakira@gmail.com>
2022-01-24 20:56:26 +0900

    /spot がパラメータを遅れるように修正

--- src/stores/models/spot.js
+++ src/stores/models/spot.js
@@ -5,23 +5,35 @@ export default {
   strict: true,
   namespaced: true,
   state: {
-    spots: [],
+    spots: {
+      spots: [],
+      totalCount: 0,
+    },
     isLoading: false,
+  },
+  getters: {
+    all: state => state.spots.spots,
+    size: state => state.spots.spots.length,
+    totalCount: state => state.spots.totalCount,
   },
   mutations: {
     setSpots(state, spots) {
-      Vue.set(state, 'spots', spots);
+      const newSpots = state.spots;
+      newSpots.spots = [...newSpots.spots, ...spots.spots];
+      newSpots.totalCount = spots.totalCount;
+
+      Vue.set(state, 'spots', newSpots);
     },
     setIsLoading(state, isLoading) {
       state.isLoading = isLoading;
     },
   },
   actions: {
-    async getSpots(context) {
+    async getSpots(context, params) {
       context.commit('setIsLoading', true);
 
       try {
-        const spots = await ApiClient.getSpots();
+        const spots = await ApiClient.getSpots(params);
 
         context.commit('setSpots', spots);
       } catch (error) {
