4ad43bc5c87e36569202db5c0107b3f19588ec70
Akira Yoshida (Noguchi) <guppy0356.nakira@gmail.com>
2022-02-08 22:21:22 +0900

    isFavorited は computed なので methods にある isFavorite に合わせる

--- src/components/organisms/campsite-list.vue
+++ src/components/organisms/campsite-list.vue
@@ -21,7 +21,7 @@
                 <span>
                   {{ campsite.name }}
                   <img
-                    v-if="isShowFavoriteMark && isFavorited(campsite)"
+                    v-if="isShowFavoriteMark && isFavorite(campsite)"
                     src="@/assets/images/user/user-plan/favorite.png"
                     width="18px"
                   >
@@ -69,9 +69,6 @@ export default {
   computed: {
     modifier() {
       return `longdivider ${this.hasChevron ? 'chevron' : ''}`;
-    },
-    isFavorited() {
-      return campsite => this.$store.getters['models/usersFavorite/findByCampsiteId'](campsite.id);
     },
   },
   mounted() {
