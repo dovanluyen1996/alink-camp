54963fe2ae7b190ec3343a09a83bee7b6231cfdf
Akira Yoshida (Noguchi) <guppy0356.nakira@gmail.com>
2022-02-21 11:54:43 +0900

    雨雲レーダータブにボタンを配置して雨雲レーダーに遷移する用に調整

--- src/components/organisms/campsites/rainy/index.vue
+++ src/components/organisms/campsites/rainy/index.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="rainy-tab">
     <div class="rainy-wrapper">
-      <!-- TODO: Implement Map when implement logic -->
-
-      <img src="@/assets/images/rainy.png">
+      <v-ons-button
+        @click="goToMap()"
+      >
+        雨雲の動きを確認
+      </v-ons-button>
     </div>
   </div>
 </template>
@@ -11,6 +13,16 @@
 <script>
 export default {
   name: 'CampsiteRainyTab',
+  computed: {
+    campsite() {
+      return this.$store.getters['campsite/choosenCampsite'];
+    },
+  },
+  methods: {
+    goToMap() {
+      this.$helpers.openPageByUrl(`${process.env.MAP_URL}?lat=${this.campsite.latitude}&lon=${this.campsite.longitude}`);
+    },
+  },
 };
 </script>
 
@@ -20,10 +32,6 @@ export default {
       padding: 20px 10px 30px;
       margin-bottom: 30px;
       background-color: #fff;
-
-      img {
-        width: 100%;
-      }
     }
   }
 </style>
