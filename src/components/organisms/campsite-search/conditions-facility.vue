bd04417a6b5ea0e8629e370d3f0fc84af2e4211e
ri-kawakami <toshihiro.kawakami@rights-s.jp>
2022-03-03 21:37:34 +0900

    キャンプ場検索 施設条件のリクエストデータ設定を変更

--- src/components/organisms/campsite-search/conditions-facility.vue
+++ src/components/organisms/campsite-search/conditions-facility.vue
@@ -62,47 +62,38 @@ export default {
     return {
       locations: [
         {
-          value: 1,
           text: '河畔',
           checked: false,
         },
         {
-          value: 2,
           text: '湖畔',
           checked: false,
         },
         {
-          value: 3,
           text: '海岸',
           checked: false,
         },
         {
-          value: 4,
           text: '臨海',
           checked: false,
         },
         {
-          value: 5,
           text: '林間',
           checked: false,
         },
         {
-          value: 6,
           text: '山間',
           checked: false,
         },
         {
-          value: 7,
           text: '高原',
           checked: false,
         },
         {
-          value: 8,
           text: '公園',
           checked: false,
         },
         {
-          value: 9,
           text: 'その他',
           checked: false,
         },
@@ -115,103 +106,103 @@ export default {
           checked: false,
         },
         {
-          value: 2,
+          value: 1,
           name: 'parking',
           text: '駐車場有',
           checked: false,
         },
         {
-          value: 3,
+          value: 1,
           name: 'near_station',
           text: '駅から徒歩5分以内',
           checked: false,
         },
         {
-          value: 4,
+          value: 1,
           text: 'オートキャンプ可能',
           name: 'style_auto_camping',
           checked: false,
         },
         {
-          value: 5,
+          value: 1,
           text: 'デイキャンプ可能',
           name: 'style_day_camping',
           checked: false,
         },
         {
-          value: 6,
+          value: 1,
           text: 'トレーラーハウス有',
           name: 'facility_trailer_house',
           checked: false,
         },
         {
-          value: 7,
+          value: 1,
           text: 'バンガローなどの宿泊施設有',
           name: 'facility_bungalow',
           checked: false,
         },
         {
-          value: 8,
+          value: 1,
           name: 'facility_permanent_tent',
           text: '常設テント・ティピー・パオ有',
           checked: false,
         },
         {
-          value: 9,
+          value: 1,
           name: 'facility_ac',
           text: 'AC電源付きサイト有',
           checked: false,
         },
         {
-          value: 10,
+          value: 1,
           name: 'facility_laundry',
           text: 'ランドリー有',
           checked: false,
         },
         {
-          value: 11,
+          value: 1,
           name: 'facility_flush_toilet',
           text: '水洗トイレ有',
           checked: false,
         },
         {
-          value: 12,
+          value: 1,
           name: 'facility_shower',
           text: '温水シャワー有',
           checked: false,
         },
         {
-          value: 13,
+          value: 1,
           name: 'facility_bathroom',
           text: '風呂有',
           checked: false,
         },
         {
-          value: 14,
+          value: 1,
           name: 'facility_hot_spring',
           text: '温泉有',
           checked: false,
         },
         {
-          value: 15,
+          value: 1,
           name: 'facility_barrier_free',
           text: 'バリアフリー施設有',
           checked: false,
         },
         {
-          value: 16,
+          value: [0, 2],
           name: 'rule_direct_heat',
           text: '直火可',
           checked: false,
         },
         {
-          value: 17,
+          value: [0, 2],
           name: 'rule_pets',
           text: 'ペット連れ可',
           checked: false,
         },
         {
-          value: 18,
+          value: [0, 2],
           name: 'rule_firework',
           text: '花火可',
           checked: false,
@@ -225,55 +216,55 @@ export default {
           checked: false,
         },
         {
-          value: 2,
+          value: 1,
           name: 'spot_fishing',
           text: '周辺での釣り可',
           checked: false,
         },
         {
-          value: 3,
+          value: 1,
           name: 'spot_trekking',
           text: '周辺でのトレッキング可',
           checked: false,
         },
         {
-          value: 4,
+          value: 1,
           name: 'spot_star_watching',
           text: '周辺でのスターウォッチング可',
           checked: false,
         },
         {
-          value: 5,
+          value: 1,
           name: 'spot_bird_watching',
           text: '周辺でのバードウォッチング可',
           checked: false,
         },
         {
-          value: 6,
+          value: 1,
           name: 'spot_sea_bathing',
           text: '周辺での海水浴可',
           checked: false,
         },
         {
-          value: 7,
+          value: 1,
           name: 'spot_waterplay',
           text: '周辺での水遊び可',
           checked: false,
         },
         {
-          value: 8,
+          value: 1,
           name: 'spot_hot_spring',
           text: '周辺の温泉施設',
           checked: false,
         },
         {
-          value: 9,
+          value: 1,
           name: 'spot_farm',
           text: '周辺の観光牧場',
           checked: false,
         },
         {
-          value: 10,
+          value: 1,
           name: 'spot_leisure',
           text: '周辺のレジャー施設',
           checked: false,
@@ -308,11 +299,11 @@ export default {
 
       // 施設情報
       // チェックがあればパラメーターに含める
-      this.selectedFacilities.forEach((f) => { params[f.name] = 1; });
+      this.selectedFacilities.forEach((f) => { params[f.name] = f.value });
 
       // 周辺情報
       // チェックがあればパラメーターに含める
-      this.selectedNearbies.forEach((n) => { params[n.name] = 1; });
+      this.selectedNearbies.forEach((n) => { params[n.name] = n.value });
 
       return params;
     },
