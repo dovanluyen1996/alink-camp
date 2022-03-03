a5090cf4ce6194f91996a42b1e23e5c3f997c077
vanluyen <dovan.luyen@rights-s.jp>
2022-03-03 17:21:56 +0700

    show_weather_when_user_purchased

---
+++ src/components/organisms/campsite-search/not-purchased.vue
@@ -0,0 +1,38 @@
+<template>
+  <div
+    class="conditions-box"
+  >
+    <div class="conditions-title">
+      課金LPの魅力バナー
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/scss/_variables.scss';
+
+.conditions-box {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.conditions-title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 297px;
+  height: 136px;
+  font-size: 16px;
+  color: $color-subtext;
+  background: $color-white;
+  border: 1px solid #afafaf;
+  border-radius: 10px;
+}
+
+</style>
