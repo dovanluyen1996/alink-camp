16de8025760ba5b576d4287669f48802449a735b
Natsuru Shirose <natsuru.shirose@rights-s.jp>
2022-03-19 17:45:20 +0900

    計画新規 タスク登録 bug fix

--- src/components/organisms/edit-dialog-task.vue
+++ src/components/organisms/edit-dialog-task.vue
@@ -47,10 +47,16 @@ export default {
       },
     },
   },
+  watch: {
+    isVisible() {
+      if (this.isVisible) this.newTask = this.tasks[this.targetAt];
+    },
+  },
   methods: {
     closeTask() {
       this.$emit('update:tasks', { ...this.tasks, [this.targetAt]: this.newTask });
       this.$emit('close');
+      this.newTask = '';
     },
   },
 };
