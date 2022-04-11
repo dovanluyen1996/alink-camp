6134425dd7ebb8b0bd02959f4512353adf339384
Natsuru Shirose <natsuru.shirose@rights-s.jp>
2022-04-11 11:05:39 +0900

    アプリ起動ときに課金判定

--- src/mixins/checkCompleteRegistration.js
+++ src/mixins/checkCompleteRegistration.js
@@ -1,14 +1,29 @@
 import AppTabbar from '@/views/app-tabbar';
 import IdfaAaidPlugin from '@/config/idfa';
+import PurchaseInformation from '@/views/purchase-information';
 
 export default {
   methods: {
     async checkBeforeGoToAppTabbar() {
+      if (!BuildInfo.debug) this.checkChargedStatus();
       await IdfaAaidPlugin.init();
       await this.$helpers.callGeolocationPermission();
       await this.$helpers.callPushNotificationPermission();
 
       this.$store.dispatch('appNavigator/push', AppTabbar);
+    },
+    async checkChargedStatus() {
+      Purchases.getPurchaserInfo(
+        async(purchaserInfo) => {
+          const isCharged = Object.entries(purchaserInfo.entitlements.active).length > 0;
+          this.$store.dispatch('purchase/setIsPurchased', isCharged);
+        },
+        () => {
+          // 全画面共通のエラーメッセージを表示したい
+          this.checkChargedStatusErrorVisible = true;
+          this.$store.dispatch('appNavigator/reset', PurchaseInformation);
+        },
+      );
     },
   },
 };
